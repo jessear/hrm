d8510b76ae053feea0617d7cc5c10973fe276fc2
jessear <13262623319@163.com>
2017-08-15 23:19:57 +0800

    添加quartz定时任务

---
+++ src/com/jesse/task/MyTask.java
@@ -0,0 +1,25 @@
+package com.jesse.task;
+
+import com.jesse.bean.User;
+import com.jesse.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
+import java.util.List;
+
+/**
+ * Created by Jesse on 2017/8/15 0015.
+ */
+public class MyTask {
+
+    @Autowired
+    @Qualifier("userService")
+    private UserService userService;
+
+    public void execute(){
+        List<User> users=userService.findUser(null,null);
+        for(User user :users){
+            System.out.println(user.getUsername());
+        }
+    }
+}
